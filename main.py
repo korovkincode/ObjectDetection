def442a14dcd96b3f163567fc804da9391721495
DELL <nickorovkinwork@gmail.com>
2021-06-10 12:26:13 +0300

    Add people counter

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ def detection():
 	#You can install this model from https://github.com/matterport/Mask_RCNN/releases/
 	image.load_model('mask_rcnn_coco.h5')
 	target_class = image.select_target_classes(person = True)
-	image.segmentImage(show_bboxes = True, segment_target_classes = target_class, image_path = 'city.jpg', output_image_name = 'result.jpg')
+	res = image.segmentImage(
+		show_bboxes = True,
+		segment_target_classes = target_class,
+		image_path = 'city.jpg',
+		output_image_name = 'result.jpg'
+	)
+	people_cnt = 0
+	for score in res[0]['scores']:
+		if score >= 0.6:
+			people_cnt += 1
+	print(people_cnt)
 
 detection()
